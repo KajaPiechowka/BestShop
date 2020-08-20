0a32b96f167b9c1d790918f94a0f566eff170d47
Kaja Piechówka <kaja.piechowka@gmail.com>
2020-08-20 14:53:22 +0200

    kalkulator js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   /**Kalkulator */
   const calculator = document.getElementById("calculator");
+  const calculatorLeft = calculator.querySelector(".calculator-left");
   /**input */
   const productsQuantity = calculator.querySelector("#quantity");
   const months = calculator.querySelector("#months");
   /**dropdown */
   const packageDropdown = calculator.querySelector("#packageDropdown");
-  const packageList = packageDropdown.querySelectorAll(
-    "option:not(:first-child)"
-  );
-
   /**checkboxy */
   const accoutingCheckbox = calculator.querySelector("#accountingChbx");
   const terminalCheckbox = calculator.querySelector("#terminalChbx");
@@ -22,19 +19,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const terminal = calculator.querySelector("#terminal");
   /**niebieska lista */
   const total = calculator.querySelector("#total");
+  const totalPrice = calculator.querySelector("output#total-price");
+  /**total */
 
   /**input event */
 
   const showProducts = () => {
     if (productsQuantity.value > 0) {
       products.style.display = "flex";
-
       products.firstElementChild.nextElementSibling.innerHTML = `${productsQuantity.value}*$0,5`;
       products.lastElementChild.innerHTML = `$${productsQuantity.value * 0.5}`;
-      products.dataset["price"] = `${productsQuantity.value * 0.5}`;
     } else {
       products.style.display = "none";
-      products.removeAttribute("data-price");
     }
   };
   productsQuantity.addEventListener("change", showProducts);
@@ -43,12 +39,11 @@ document.addEventListener("DOMContentLoaded", function () {
       orders.style.display = "flex";
       orders.firstElementChild.nextElementSibling.innerHTML = `${months.value}*$0,5`;
       orders.lastElementChild.innerHTML = `$${months.value * 0.25}`;
-      orders.dataset["price"] = `${productsQuantity.value * 0.25}`;
     } else {
       orders.style.display = "none";
-      orders.removeAttribute("data-price");
     }
   };
+
   productsQuantity.addEventListener("change", showProducts);
   months.addEventListener("change", showMonths);
 
@@ -58,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
     package.style.display = "flex";
     package.firstElementChild.nextElementSibling.innerHTML = `${packageDropdown.value}`;
     package.lastElementChild.innerHTML = `$${
-      packageDropdown.options[packageDropdown.selectedIndex].dataset["price"] //to trzeba przerobić eby dodawał się data-price
+      packageDropdown.options[packageDropdown.selectedIndex].dataset["price"]
     }`;
   });
 
@@ -68,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (accoutingCheckbox.checked === true) {
       accounting.style.display = "flex";
     } else {
-      accounting.style.display = "none"; //dodaj data-price
+      accounting.style.display = "none";
     }
     showValue(accounting);
   });
@@ -76,12 +71,12 @@ document.addEventListener("DOMContentLoaded", function () {
     if (terminalCheckbox.checked === true) {
       terminal.style.display = "flex";
     } else {
-      terminal.style.display = "none"; //dodaj data-price
+      terminal.style.display = "none";
     }
   });
   /**total event */
-  let sum = 0; //suma wszystkich data-price
+  let sum = 0;
   const showValue = function (el) {
-    console.log(el.dataset["price"]); // zrób funkcję uniwersalą i dodaj ją do eventów
+    console.log(el.dataset["price"]);
   };
 });
